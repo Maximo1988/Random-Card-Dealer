541316a0cbfa6130aa4469d6d1347f015f663734
Maximo1988 <max_bd88@hotmail.com>
2022-02-21 08:53:06 +0000

    actualizando contenido

--- src/app.js
+++ src/app.js
@@ -9,3 +9,34 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+document.querySelector(".deck").classList.add(generateRandomSuit());
+document.querySelector(".number").innerHTML = generateRandomNumber();
+document.querySelector("#button");
+
+let generateRandomNumber = () => {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexN = Math.floor(Math.random() * number.length);
+  return number[indexN];
+};
+
+let generateRandomSuit = () => {
+  let deck = ["♥", "♣", "♦", "♠"];
+  let indexS = Math.floor(Math.random() * deck.length);
+
+  return deck[indexS];
+};
