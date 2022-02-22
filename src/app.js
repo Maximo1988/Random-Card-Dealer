218e505a11ae038e4a5ff8d2ed6607311b9ddb33
Maximo1988 <max_bd88@hotmail.com>
2022-02-22 00:31:43 +0000

    contenido completo

--- src/app.js
+++ src/app.js
@@ -8,35 +8,63 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-};
 
-document.getElementsByTagName(".deck").innerHTML(generateRandomDeck());
-document.getElementsByTagName(".number").innerHTML = generateRandomNumber();
-document.getElementsByTagName("#button").innerHTML;
+  let generateRandomNumber = () => {
+    let number = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+    ];
+    let FirstNumber = Math.floor(Math.random() * number.length);
+    return number[FirstNumber];
+  };
 
-let generateRandomNumber = () => {
-  let number = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let FirstNumber = Math.floor(Math.random() * number.length);
-  return number[FirstNumber];
-};
+  let generateRandomDeck = () => {
+    let Deck = ["♥", "♣", "♦", "♠"];
+    let FirstDeck = Math.floor(Math.random() * Deck.length);
 
-let generateRandomDeck = () => {
-  let Deck = ["♥", "♣", "♦", "♠"];
-  let FirstDeck = Math.floor(Math.random() * Deck.length);
+    return Deck[FirstDeck];
+  };
 
-  return deck[FirstDeck];
+  let randomdeck = generateRandomDeck();
+  document.getElementById("deck-top").innerText = randomdeck;
+  document.getElementById("deck-bottom").innerText = randomdeck;
+  let randomnumber = generateRandomNumber();
+  document.getElementById("number").innerHTML = randomnumber;
+
+  if (randomdeck == "♦" || randomdeck == "♥") {
+    document.getElementById("deck-top").style.color = "red";
+    document.getElementById("deck-bottom").style.color = "red";
+  } else {
+    document.getElementById("deck-top").style.color = "black";
+    document.getElementById("deck-bottom").style.color = "black";
+  }
+
+  let button = document.getElementById("button");
+  console.log(button);
+  button.addEventListener("click", () => {
+    randomdeck = generateRandomDeck();
+    randomnumber = generateRandomNumber();
+    document.getElementById("deck-top").innerText = randomdeck;
+    document.getElementById("deck-bottom").innerText = randomdeck;
+    document.getElementById("number").innerHTML = randomnumber;
+
+    if (randomdeck == "♦" || randomdeck == "♥") {
+      document.getElementById("deck-top").style.color = "red";
+      document.getElementById("deck-bottom").style.color = "red";
+    } else {
+      document.getElementById("deck-top").style.color = "black";
+      document.getElementById("deck-bottom").style.color = "black";
+    }
+  });
 };
